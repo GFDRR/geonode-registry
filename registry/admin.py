f7abf626ceb17a88f8a551d3aa4e828bf395b4fe
Ariel Nunez <ingenieroariel@gmail.com>
2011-12-14 16:46:56 -0500

    Added filter for date and instance in status reports

--- registry/admin.py
+++ registry/admin.py
@@ -8,6 +8,7 @@ class GeoNodeStatusAdmin(admin.ModelAdmin):
     list_display = ('instance', 'layer_count', 'faulty_layers_count',
                      'map_count', 'faulty_maps_count',
                      'backup_date', 'created_at')
+    list_filter = ('instance', 'created_at')
 
 class FaultyLayerAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'url', 'reason')
