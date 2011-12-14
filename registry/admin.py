15cca7213d9149ba701500fb2bae1842705e406e
Ariel Nunez <ingenieroariel@gmail.com>
2011-12-14 16:44:19 -0500

    Typo in faulty_maps_count

--- registry/admin.py
+++ registry/admin.py
@@ -5,7 +5,9 @@ class GeoNodeInstanceAdmin(admin.ModelAdmin):
     list_display = ('name', 'url', 'created_at')
 
 class GeoNodeStatusAdmin(admin.ModelAdmin):
-    list_display = ('instance', 'layer_count', 'faulty_layers_count', 'map_count', 'faulty_maps', 'backup_date', 'created_at')
+    list_display = ('instance', 'layer_count', 'faulty_layers_count',
+                     'map_count', 'faulty_maps_count',
+                     'backup_date', 'created_at')
 
 class FaultyLayerAdmin(admin.ModelAdmin):
     list_display = ('name', 'instance', 'url', 'reason')
